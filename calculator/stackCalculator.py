041146e16d0371886332d6b26dfb7a8db21ebcaf
jaeseokk <kakaokokoa9971@gmail.com>
2023-09-30 16:35:25 +0900

    Create stackCalculator.py
    make stackCalculator by python
    with postfix converter
    without bracket

---
+++ calculator/stackCalculator.py
@@ -0,0 +1,42 @@
+formular = input("Enter your formular: ")
+
+oper = {"*":1,"/":1,"+":0,"-":0}
+stack = []
+number = ""
+
+postfix = []
+
+for s in formular:
+    if s in oper.keys():
+        postfix.append(int(number))
+        while stack:
+            if oper[stack[-1]] < oper[s]:
+                break
+            else:
+                postfix.append(stack.pop())
+
+        stack.append(s)
+        number = ""
+    else:
+        number += s
+postfix.append(int(number))
+while stack:
+    postfix.append(stack.pop())
+print(postfix)
+
+calStack = []
+for c in postfix:
+    if c in oper.keys():
+        b,a = calStack.pop(), calStack.pop()
+        if c == "*":
+            calStack.append(a*b)
+        elif c == "/":
+            calStack.append(a/b)
+        elif c == "+":
+            calStack.append(a+b)
+        elif c == "-":
+            calStack.append(a-b)
+    else:
+        calStack.append(c)
+
+print(calStack)
