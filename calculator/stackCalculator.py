e7fa583b738985196e6e78c886631ff26504622d
jaeseokk <kakaokokoa9971@gmail.com>
2023-10-11 19:39:20 +0900

    Update stackCalculator.py

--- calculator/stackCalculator.py
+++ calculator/stackCalculator.py
@@ -1,11 +1,8 @@
 import Calculator
 
 formular = Calculator.inputFormular()
-
 oper = {"*":1,"/":1,"+":0,"-":0}
-
 postfix = Calculator.convertToPostfix(formular,oper)
-
 answer = Calculator.calculatePostfix(postfix,oper)
 
 print(*answer)
