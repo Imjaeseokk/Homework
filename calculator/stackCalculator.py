f7889a22f1ab86ce96891de701f5865a16fe8444
jaeseokk <kakaokokoa9971@gmail.com>
2023-10-11 19:35:00 +0900

    Update stackCalculator.py

--- calculator/stackCalculator.py
+++ calculator/stackCalculator.py
@@ -1,49 +1,11 @@
 import Calculator
 
-
 formular = Calculator.inputFormular()
 
 oper = {"*":1,"/":1,"+":0,"-":0}
 
-# stack = []
-# number = ""
-#
-# postfix = []
-
-# for s in formular:
-#     if s in oper.keys():
-#         postfix.append(int(number))
-#         while stack:
-#             if oper[stack[-1]] < oper[s]:
-#                 break
-#             else:
-#                 postfix.append(stack.pop())
-#
-#         stack.append(s)
-#         number = ""
-#     else:
-#         number += s
-# postfix.append(int(number))
-# while stack:
-#     postfix.append(stack.pop())
-
 postfix = Calculator.convertToPostfix(formular,oper)
 
 answer = Calculator.calculatePostfix(postfix,oper)
-
-# calStack = []
-# for c in postfix:
-#     if c in oper.keys():
-#         b,a = calStack.pop(), calStack.pop()
-#         if c == "*":
-#             calStack.append(a*b)
-#         elif c == "/":
-#             calStack.append(a/b)
-#         elif c == "+":
-#             calStack.append(a+b)
-#         elif c == "-":
-#             calStack.append(a-b)
-#     else:
-#         calStack.append(c)
 
 print(*answer)
